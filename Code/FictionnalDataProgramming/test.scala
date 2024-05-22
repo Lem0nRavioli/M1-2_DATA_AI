1a42f15ff29bab551f4dc51c4cb48523d2f0cb02
Guillaume Saulnier <hirona_mj@hotmail.fr>
2024-05-22 09:10:13 +0200

    scala whatever

---
+++ Code/FictionnalDataProgramming/test.scala
@@ -0,0 +1,13 @@
+import scala.io._
+object Main extends App {
+    println("Hello world")
+    // ex1
+    // val n = 5
+    // val arr = new Array[String](n)
+    // for (i <- 0 until n) {
+    // arr(i) = "hello world"
+    // }
+
+
+
+}
